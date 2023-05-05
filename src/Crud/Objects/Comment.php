f6b149d4f4372718a962fc96d0410ec2c0f86016
Kyle Lawrence <kyle@designanddevelop.io>
2023-05-05 17:51:59 -0500

    first commit

--- src/Crud/Objects/Comment.php
+++ src/Crud/Objects/Comment.php
@@ -15,7 +15,7 @@ class Comment extends OriginalComment
     public function __construct(
         protected object $apiObject,
         protected ?string $board_id,
-        protected ?string $parent_resource_id,
+        protected string $parent_resource_id,
         protected $client = new InfinityService(),
     ) {
         parent::__construct($apiObject, $board_id);
